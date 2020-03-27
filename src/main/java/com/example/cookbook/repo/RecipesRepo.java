f0255871a4c5dc18a2748bd57c7193c7e096cb63
JulianaOsi <julia.osintseva15@gmail.com>
2020-03-28 00:55:49 +0500

    set max upload file size

--- src/main/java/com/example/cookbook/repo/RecipesRepo.java
+++ src/main/java/com/example/cookbook/repo/RecipesRepo.java
@@ -1,7 +1,8 @@
 package com.example.cookbook.repo;
 
 import com.example.cookbook.domain.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface RecipesRepo extends CrudRepository<Recipe, Long> {
+public interface RecipesRepo extends JpaRepository<Recipe, Long> {
 }
