6a4a3fc621f7db56c4b550a567eb32f3ba95e7fb
hacksocto <hacksocto@gmail.com>
2020-04-21 15:26:40 +0500

    added difference between pages view to authorized and unauthorized user. Added account page

--- src/main/java/com/example/cookbook/controller/RegistrationController.java
+++ src/main/java/com/example/cookbook/controller/RegistrationController.java
@@ -3,10 +3,12 @@ package com.example.cookbook.controller;
 import com.example.cookbook.domain.User;
 import com.example.cookbook.service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 import java.util.Map;
 
 @Controller
@@ -18,6 +20,14 @@ public class RegistrationController {
     @GetMapping("/registration")
     public ModelAndView registration() {
         return new ModelAndView("registration");
+    }
+
+    @PostMapping("/user/account")
+    public ModelAndView getAccountPage(
+            @AuthenticationPrincipal User user,
+            Map<String, Object> model) {
+        model.put("user", user);
+        return new ModelAndView("/account");
     }
 
     @PostMapping("/registration")
